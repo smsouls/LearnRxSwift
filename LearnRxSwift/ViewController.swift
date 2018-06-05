fbf413dbd69ace56d7b0a4ca6db0e4dd3c4b84a2
123 <1527340081@qq.com>
2018-06-05 09:24:38 +0800

    新建一个新的空白工程

---
+++ LearnRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnRxSwift
+//
+//  Created by 123 on 2018/6/5.
+//  Copyright © 2018年 NoName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'LearnRxSwift' do
+  use_frameworks!
+
+pod 'RxSwift',    '~> 4.0.0'
+pod 'RxCocoa',    '~> 4.0.0'
+
+end
+
